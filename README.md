915749ff3f7dcd58e474ed7e5660357d0bdd0cbf
Yadier Martinez <yadieralexander19@gmail.com>
2023-10-17 15:14:10 -0400

    Some changes were made

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# React + Vite
+# Salary Discount Calculator
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Web page that will help you to calculate your salary discounts in DR.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,43 @@
+import {
+  Button,
+  Container,
+  Divider,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+
 function App() {
-  return <div>Salary Discount Calculator</div>;
+  return (
+    <Container minWidth="4xl" py="20px">
+      <Heading
+        mb="20px"
+        as="h1"
+        bgColor="rgba(13,110,253,255) "
+        color="white"
+        padding="4px 2px"
+        textAlign="center"
+        verticalAlign="center"
+        borderRadius="5px"
+      >
+        Salary Discount Calculator
+      </Heading>
+      <Heading as="h2" mb="20px">
+        Input
+      </Heading>
+      <Text my="10px">Monthly Salary (in DOP)* </Text>
+      <Input placeholder="Monthly Salary" type="number" />
+      <Text my="10px">Bonifications (in DOP)* </Text>
+      <Input placeholder="Bonifications" type="number" />
+      <Text my="10px">Extra Hours (in DOP)* </Text>
+      <Input placeholder="Extra Hours" type="number" />
+      <Button bgColor="green.400" color="white" my="10px">
+        Calculate
+      </Button>
+      <Divider />
+      <Heading>Results: </Heading>
+    </Container>
+  );
 }
 
 export default App;
